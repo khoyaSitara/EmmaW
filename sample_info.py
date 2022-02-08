5af59352a9046a17d3f1c8b36d937e79d21e7ba8
AshishSharma2006 <99271761+AshishSharma2006@users.noreply.github.com>
2022-02-08 21:16:35 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -11,23 +11,23 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = True
 
 # Admins, Channels & Users
-ADMINS = [1997411088 , 1090694685]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [1571060413]
+CHANNELS = [-1001739121034, -1001730999908, '@eliteflixbyashishsharma']
 AUTH_USERS = []
-AUTH_CHANNEL = '@Before_Dm'
+AUTH_CHANNEL = '@eliteflixbyashishsharma2'
 
 # MongoDB informatiin
-DATABASE_URI = "mongodb+srv://ak1046516:thakur#12@cluster0.wpso2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-DATABASE_NAME = 'Samnantha'
-COLLECTION_NAME = 'Telegram_files'  # if u are using the same database, then use different collection name for each bot
+DATABASE_URI = "mongodb+srv://Ashish:5412ascs@eliteflix.kw22u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_NAME = 'Eliteflix'
+COLLECTION_NAME = 'Eliteflix'  # if u are using the same database, then use different collection name for each bot
 
 #MyChanges
-SUPPORT_CHAT = '@Before_Dm'
+SUPPORT_CHAT = '@eliteflix_chat'
 P_TTI_SHOW_OFF = True
 SINGLE_BUTTON = False 
 SPELL_CHECK_REPLY = False
 MAX_LIST_ELM = 10
-UPSTREAM_REPO ='https://github.com/abhijeetkumar2004/EvaMaria'
+UPSTREAM_REPO ='https://github.com/AshishSharma2006/Eliteflix'
 MELCOW_NEW_USERS = False
 
 
