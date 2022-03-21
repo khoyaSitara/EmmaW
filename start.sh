33eeefdb6da5e41aa465f884d87d7e6cc332ee60
AshishSharma2006 <99271761+AshishSharma2006@users.noreply.github.com>
2022-03-21 14:31:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/AshishSharma2006/Eliteflix
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Eliteflix
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
