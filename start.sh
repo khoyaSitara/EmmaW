b209f237b5855541d8acd881d10166f28c9ea297
AshishSharma2006 <99271761+AshishSharma2006@users.noreply.github.com>
2022-04-30 22:43:25 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eliteflix
 fi
-cd /EvaMaria
+cd /Eliteflix
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
